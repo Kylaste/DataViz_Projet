ec136a6d702d1911bea161f6290639840e5d901c
kylaste <kylaste@yahoo.fr>
2018-01-09 11:12:45 +0100

    transition matrice tgv/matrice intercité

--- data/changer.py
+++ data/changer.py
@@ -66,12 +66,29 @@ with open("regularite-mensuelle-intercites.csv", "r") as fichier:
     with open("trajets_intercites.csv", "w") as denFile:
         fileWriter = csv.writer(denFile, delimiter=",")
         fileWriter.writerow(
-            ["source", "target", "weight"])
+            ["source", "target", "weight", "annee", "mois", "commentaire"])
         compteur = 0
+        moyenne = {}
+        nbMois = {}
         for row in cr:
-            if( compteur > 0):
-                fileWriter.writerow([row[3], row[4], row[9]])
-            compteur = compteur + 1
+                if( compteur > 0):
+                    datetime_object = datetime.strptime(row[1], '%Y-%m')
+                    id = row[3]+"-"+row[4]+"-"+str(datetime_object.year)
+                    
+                    if(row[9] != ""):
+                        if( id in moyenne):
+                            moyenne[id] += float(row[9])
+                            nbMois[id] += 1
+                        else:
+                            moyenne[id] = float(row[9])
+                            nbMois[id] = 1
+                    fileWriter.writerow([row[3], row[4], row[9], datetime_object.year, datetime_object.month, ""])
+                compteur = compteur + 1
+        for row in moyenne:
+            
+            info = row.split('-')
+            moyenne[row] =round(float(moyenne[row])/nbMois[row], 2)
+            fileWriter.writerow([info[0], info[1], moyenne[row], info[2], 0, ""])
             
 
 
